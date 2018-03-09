5d5d14ae46424b4d553243ac88baf784232eaa65
Yannis Piot Pilot <yannis@piotpilot.eu>
2018-03-09 15:40:29 +0100

    Add exo 1 et jean :D

---
+++ com/beweb/lunel/programmation/exosEnVrac/algo/Exercice1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.programmation.exosEnVrac.algo;
+
+/**
+ * Init deux variables booléennes empty et fully
+ * @author yannis
+ */
+public class Exercice1 {
+    
+    
+    public static void launch(){
+        // Déclare mes variables
+        boolean empty = false;
+        boolean fylly = true;
+        
+        // Permet de voir si bien exec
+        System.out.println("Exo 1 : La cabane au fond Dujardin ( Jean ) !");
+    }
+}
